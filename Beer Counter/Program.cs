d4fe8edbbb6b9fe89d7dc8a2578b42b1cd73598f
Jorge Faria <JorgeFaria@users.noreply.github.com>
2020-03-30 17:56:49 +0100

    Beer Counter

---
+++ Beer Counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Beer_Counter
+{
+    class Program
+    {
+         
+        static void Main(string[] args)
+        {
+
+            string command = Console.ReadLine();
+
+            while (command != "End")
+            {
+                
+                
+                    string[] words = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                    Console.WriteLine(counter.BuyBeer(int.Parse(words[0])));
+                    Console.WriteLine(counter.DrinkBeer(int.Parse(words[1])));
+
+                   if(counter.beerInStock < 1)
+                    {
+                    Console.WriteLine("Seu bêbado não tens vergonha?");
+                    }
+                    
+                
+               command = Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ Beer Counter/counter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beer_Counter
+{
+    public static class counter
+    {
+
+        public static int beerDrankCount;
+        public static int beerInStock;
+        public static int BuyBeer(int BottleCount)
+        {
+            beerInStock += BottleCount;
+            return(beerInStock) ;
+        }
+
+        public static int DrinkBeer(int bottlesCount)
+        {
+            beerDrankCount += bottlesCount;
+            beerInStock -= bottlesCount;
+            return (beerDrankCount);
+        }
+    }
+}
