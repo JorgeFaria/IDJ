9edb2a21f3c55d3363c0f67c6c53001d95776f91
Jorge Faria <JorgeFaria@users.noreply.github.com>
2020-03-31 19:51:51 +0100

    Basic Maths

---
+++ BasicMath/Maths.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicMath
+{
+    class Maths
+    {
+       
+        public static string Sum(double val1, double val2)
+        {
+            double soma = val1 + val2;
+            return String.Format("{0:F2}", soma);
+        }
+        public static string Subtract(double val1, double val2)
+        {
+            double sub = val1 - val2;
+            return String.Format("{0:F2}", sub);
+        }
+        public static string Multiply(double val1, double val2)
+        {
+            double multi = val1 * val2;
+            return String.Format("{0:F2}", multi);
+        }
+        public static string Devide(double val1, double val2)
+        {
+            double dev = val1 / val2;
+            return String.Format("{0:F2}", dev);
+        }
+        public static string Percentage(double val1, double val2)
+        {
+            double dev = val1 * (val2/100);
+            return String.Format("{0:F2}", dev);
+        }
+    }
+}
+
+
---
+++ BasicMath/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BasicMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            while (command != "End")
+            {
+                string[] words = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                switch (words[0])
+                {
+                    case "Sum":
+                        Console.WriteLine(Maths.Sum(double.Parse(words[1]), double.Parse(words[2])));
+                        break;
+
+                    case "Subtract":
+                        Console.WriteLine(Maths.Subtract(double.Parse(words[1]), double.Parse(words[2])));
+                        break;
+
+                    case "Multiply":
+                        Console.WriteLine(Maths.Multiply(double.Parse(words[1]), double.Parse(words[2])));
+                        break;
+
+                    case "Devide":
+                        Console.WriteLine(Maths.Devide(double.Parse(words[1]), double.Parse(words[2])));
+                        break;
+
+                    case "Percentage":
+                        Console.WriteLine(Maths.Percentage(double.Parse(words[1]), double.Parse(words[2])));
+                        break;
+
+                    default: Console.WriteLine("Nope! Try Again!"); break;
+                }
+                command = Console.ReadLine();
+            }
+        }
+    }
+}
