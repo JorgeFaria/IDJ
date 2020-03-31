5b3310b46cf9e3e302fba43b70399d6b353dcb26
Jorge Faria <JorgeFaria@users.noreply.github.com>
2020-03-31 20:08:03 +0100

    Exercicios

---
+++ Exercicios/Plank constant/CALCULATION.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plank_constant
+{
+    
+    class CALCULATION
+    {
+        public static decimal plank = 6.62606896e-34M;
+        public static decimal pi = 3.14159M;
+        public static decimal res;
+        public string Calculo()
+        {
+            res = plank / (2 * pi);
+
+            return String.Format("{0}", res);
+        }
+    }
+}
---
+++ Exercicios/Plank constant/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Plank_constant
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(CALCULATION.res);
+        }
+    }
+}
