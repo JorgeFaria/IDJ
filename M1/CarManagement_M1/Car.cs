bf9242e477c9092afea996ed47cbf72ba839fcec
Jorge Faria <JorgeFaria@users.noreply.github.com>
2020-05-07 23:13:33 +0100

    Rim class overhaul

--- M1/CarManagement_M1/Car.cs
+++ M1/CarManagement_M1/Car.cs
@@ -6,7 +6,7 @@ using System.Text;
 namespace CarManagement_M1
     
 {
-    public class Car : Rim
+    public class Car 
     {
         private bool hit=false;
 
@@ -17,18 +17,27 @@ namespace CarManagement_M1
         public int Health { get; set; }
         public double Speed { get; set; }
 
-        public Car(int armor, string make, string model, Color color, int health, float speed, int rim_health, int size, string type, Color rim_color) : base(rim_health, size, type, rim_color)
+        public Car(int armor, string make, string model, Color color, int health, float speed)
         {
             this.Armor = armor;
             this.Make = make;
             this.Model = model;
             this.Color = color;
             this.Health = health;
-
-            //    (Axle Ratio x v x Transmission Ratio x 336.13) / Tire Diameter ] =  [(3.73 x 65 x 1.00 x 336.13) / 31 ] = [2628]
-            //Note: 336.13 is used to convert the result to RPM = [63360 inches per mile / (60 minutes per hour x Pi.)]
         }
+        public Car()
+        {
+            this.Armor = 100;
+            this.Make = "Make";
+            this.Model = "Dafaul";
+            this.Color = Color.White;
+            this.Health = 100;
+            
 
+
+        }
+        //    (Axle Ratio x v x Transmission Ratio x 336.13) / Tire Diameter ] =  [(3.73 x 65 x 1.00 x 336.13) / 31 ] = [2628]
+        //Note: 336.13 is used to convert the result to RPM = [63360 inches per mile / (60 minutes per hour x Pi.)]
         public void Checkspeed(int size)
         {
             double v = 0.00595 * (1000 * size);
--- M1/CarManagement_M1/Rim.cs
+++ M1/CarManagement_M1/Rim.cs
@@ -5,22 +5,39 @@ using System.Text;
 
 namespace CarManagement_M1
 {
-    public class Rim
+    public class Rim : Car
     {
-
         public int RimHealth { get; set; }
         public int Size { get; set; }
-        public string Type { get; set; }
         public Color RimColor { get; set; }
+        public enum RimTypes { Offroad, Sport, Drag}
 
-        public Rim(int rim_health, int size, string type, Color rim_color)
+        public Dictionary<RimTypes, int> RimType = new Dictionary<RimTypes, int>();
+
+        public Rim(int rim_health, int size,Dictionary<RimTypes, int> rimtype, Color rim_color)
         {
-           
+
             this.RimHealth = rim_health;
             this.Size = size;
-            this.Type = type;
+            // this.Type = type;
             this.RimColor = rim_color;
+            this.RimType = rimtype;
         }
+
+        public Rim()
+        {
+            this.RimHealth = 100;
+            this.Size = 15;
+            this.RimColor = Color.White;
+            this.RimType = new Dictionary<Rim.RimTypes, int>
+            {
+                {RimTypes.Offroad,0},
+                {RimTypes.Sport,0},
+                {RimTypes.Drag ,0}
+            };
         }
+
     }
+
+}
 
